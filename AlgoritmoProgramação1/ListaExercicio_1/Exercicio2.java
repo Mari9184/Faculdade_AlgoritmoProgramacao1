2945702fbfde74655a7510cf63723f7366f7d39b
Mariane Santana da Silva <marianess638@gmail.com>
2026-03-11 14:58:00 -0300

    Add Exercicio2.java with age and height output

---
+++ AlgoritmoProgramação1/ListaExercicio_1/Exercicio2.java
@@ -0,0 +1,17 @@
+//Mariane Santana da Silva RA:1143431607
+
+import java.lang.Math;
+import java.util.Scanner;
+class Main {
+    public static void main (String [] args){
+        Scanner ler = new Scanner(System.in
+
+        // 2° Exercício
+                                  
+        int Idade=1;
+        double Altura=1.61
+        
+        String Status= "Presente";
+        System.out.printf("\nTenho %d anos e %f de altura e estive %s na aula de AP1 no dia 25.02.2026\n",Idade, Altura,Status);
+    }
+}
