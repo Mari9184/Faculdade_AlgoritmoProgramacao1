40d1b2337e2efc7e69caa8b87dfbcdf0ddb0a5c1
Mariane Santana da Silva <marianess638@gmail.com>
2026-03-11 14:59:53 -0300

    Add Java program for multiplication table of 5

---
+++ AlgoritmoProgramação1/ListaExercicio_1/Exercicio3.java
@@ -0,0 +1,16 @@
+//Mariane Santana da Silva RA:1143431607
+
+import java.util.Scanner;
+
+class Main {
+    public static void main (String [] args){
+        Scanner ler = new Scanner(System.in);
+
+        //  3° Exercício - Tabuada
+      
+        int num=5; 
+        System.out.println("\nTABUADA DO 5");
+        for (int i=1 ; i <=10; i++){System.out.printf("%d * %d = %d%n\n",num,i,num*i);}
+
+    }
+}
